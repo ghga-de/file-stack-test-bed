d770997e785f47e747982a9e6a10fce87c6d9b07
Thomas J. Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2022-11-30 18:46:50 +0000

    The Changes (Part Two)

--- tests/fixtures/local_fixtures.py
+++ tests/fixtures/local_fixtures.py
@@ -15,18 +15,12 @@
 
 import hashlib
 import math
-import time
 from dataclasses import dataclass
-from datetime import datetime
 
 import pytest_asyncio  # type: ignore
 import requests  # type: ignore
-from ghga_event_schemas import pydantic_ as event_schemas  # type: ignore
 from ghga_service_chassis_lib.utils import big_temp_file  # type: ignore
-from hexkit.custom_types import JsonObject  # type: ignore
-from hexkit.providers.akafka import KafkaEventPublisher  # type: ignore
-from hexkit.providers.akafka.testutils import EventRecorder  # type: ignore
-from hexkit.providers.s3 import S3Config, S3ObjectStorage  # type: ignore
+from hexkit.providers.s3 import S3ObjectStorage  # type: ignore
 
 from tests.fixtures.config import CONFIG
 
@@ -43,22 +37,22 @@ class CheckablePayloadFields:
 
 
 @dataclass
-class RecordedEventFixture:
-    """Payload sent and fields that can be checked for equality"""
+class PopulatedS3Fixture:
+    """Holds S3 storage initialized with test object present and relevant metadata"""
 
-    payload: JsonObject
-    checkable_fields: CheckablePayloadFields
+    storage: S3ObjectStorage
+    file_size: int
+    checksum: str
 
 
 @pytest_asyncio.fixture
-async def local_s3_fixture() -> S3ObjectStorage:
+async def populated_s3_fixture() -> S3ObjectStorage:
     """Clean bucket and object"""
-    config = S3Config(
-        s3_endpoint_url=CONFIG.s3_endpoint_url,
-        s3_access_key_id=CONFIG.s3_access_key_id,
-        s3_secret_access_key=CONFIG.s3_access_key_id,
-    )
-    storage = S3ObjectStorage(config=config)
+
+    file_size = 20 * 1024**2
+    num_parts = math.ceil(file_size / PART_SIZE)
+    storage = S3ObjectStorage(config=CONFIG)
+
     if not await storage.does_bucket_exist(bucket_id=CONFIG.inbox_bucket):
         await storage.create_bucket(bucket_id=CONFIG.inbox_bucket)
     if await storage.does_object_exist(
@@ -67,31 +61,21 @@ async def local_s3_fixture() -> S3ObjectStorage:
         await storage.delete_object(
             bucket_id=CONFIG.inbox_bucket, object_id=CONFIG.object_id
         )
-    yield storage
 
-
-@pytest_asyncio.fixture
-async def populated_bucket_fixture(
-    local_s3_fixture: S3ObjectStorage,
-) -> event_schemas.FileUploadReceived:
-    """Generate test file and prepare event"""
-    file_size = 20 * 1024**2
-
-    num_parts = math.ceil(file_size / PART_SIZE)
     with big_temp_file(size=file_size) as random_data:
 
         checksum = hashlib.sha256(random_data.read()).hexdigest()
         # rewind file
         random_data.seek(0)
 
-        upload_id = await local_s3_fixture.init_multipart_upload(
+        upload_id = await storage.init_multipart_upload(
             bucket_id=CONFIG.inbox_bucket, object_id=CONFIG.object_id
         )
         try:
             for part_number in range(1, num_parts + 1):
 
                 part = random_data.read(PART_SIZE)
-                upload_url = await local_s3_fixture.get_part_upload_url(
+                upload_url = await storage.get_part_upload_url(
                     upload_id=upload_id,
                     bucket_id=CONFIG.inbox_bucket,
                     object_id=CONFIG.object_id,
@@ -100,59 +84,17 @@ async def populated_bucket_fixture(
                 response = requests.put(url=upload_url, data=part, timeout=2)
                 assert response.status_code == 200
 
-            await local_s3_fixture.complete_multipart_upload(
+            await storage.complete_multipart_upload(
                 upload_id=upload_id,
                 bucket_id=CONFIG.inbox_bucket,
                 object_id=CONFIG.object_id,
             )
 
         except (Exception, KeyboardInterrupt):
-            await local_s3_fixture.abort_multipart_upload(
+            await storage.abort_multipart_upload(
                 upload_id=upload_id,
                 bucket_id=CONFIG.inbox_bucket,
                 object_id=CONFIG.object_id,
             )
 
-        event = event_schemas.FileUploadReceived(
-            file_id=CONFIG.object_id,
-            submitter_public_key=CONFIG.submitter_pubkey,
-            upload_date=datetime.utcnow().isoformat(),
-            decrypted_size=file_size,
-            expected_decrypted_sha256=checksum,
-        )
-        yield event
-
-
-@pytest_asyncio.fixture
-async def publish_and_record_fixture(
-    populated_bucket_fixture: event_schemas.FileUploadReceived,
-):
-    """Publish incoming event, record outgoing response event"""
-
-    async with EventRecorder(
-        kafka_servers=CONFIG.kafka_servers, topic="file_interrogation"
-    ) as event_recorder:
-        async with KafkaEventPublisher.construct(config=CONFIG) as publisher:
-            type_ = "file_upload_received"
-            key = CONFIG.object_id
-            topic = "file_uploads"
-            await publisher.publish(
-                payload=populated_bucket_fixture.dict(),
-                type_=type_,
-                key=key,
-                topic=topic,
-            )
-        time.sleep(10)
-
-    assert len(event_recorder.recorded_events) == 1
-
-    return RecordedEventFixture(
-        payload=event_recorder.recorded_events[0].payload,
-        checkable_fields=CheckablePayloadFields(
-            file_id=CONFIG.object_id,
-            part_size=PART_SIZE,
-            content_checksum=populated_bucket_fixture.dict()[
-                "expected_decrypted_sha256"
-            ],
-        ),
-    )
+    yield PopulatedS3Fixture(storage=storage, file_size=file_size, checksum=checksum)
--- tests/test_testbed.py
+++ tests/test_testbed.py
@@ -13,22 +13,51 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest  # type: ignore
+import time
+from datetime import datetime
 
-from tests.fixtures.local_fixtures import local_s3_fixture  # noqa: F401
-from tests.fixtures.local_fixtures import populated_bucket_fixture  # noqa: F401
-from tests.fixtures.local_fixtures import publish_and_record_fixture  # noqa: F401
-from tests.fixtures.local_fixtures import RecordedEventFixture
+import pytest  # type: ignore
+from ghga_event_schemas import pydantic_ as event_schemas  # type: ignore
+from hexkit.providers.akafka import KafkaEventPublisher  # type: ignore
+from hexkit.providers.akafka.testutils import EventRecorder  # type: ignore
+
+from tests.fixtures.config import CONFIG
+from tests.fixtures.local_fixtures import populated_s3_fixture  # noqa: F401
+from tests.fixtures.local_fixtures import PopulatedS3Fixture
 
 
 @pytest.mark.asyncio
 async def test_outbound_payload(
-    publish_and_record_fixture: RecordedEventFixture,  # noqa: F811
+    populated_s3_fixture: PopulatedS3Fixture,  # noqa: F811
 ):
     """Test response event receival"""
-    payload = publish_and_record_fixture.payload
-    checkable_fields = publish_and_record_fixture.checkable_fields
 
+    event = event_schemas.FileUploadReceived(
+        file_id=CONFIG.object_id,
+        submitter_public_key=CONFIG.submitter_pubkey,
+        upload_date=datetime.utcnow().isoformat(),
+        decrypted_size=populated_s3_fixture.file_size,
+        expected_decrypted_sha256=populated_s3_fixture.checksum,
+    )
+
+    async with EventRecorder(
+        kafka_servers=CONFIG.kafka_servers, topic="file_interrogation"
+    ) as event_recorder:
+        async with KafkaEventPublisher.construct(config=CONFIG) as publisher:
+            type_ = "file_upload_received"
+            key = CONFIG.object_id
+            topic = "file_uploads"
+            await publisher.publish(
+                payload=event.dict(),
+                type_=type_,
+                key=key,
+                topic=topic,
+            )
+        time.sleep(10)
+
+    assert len(event_recorder.recorded_events) == 1
+
+    payload = event_recorder.recorded_events[0].payload
     assert "upload_date" in payload.keys()
-    assert payload["file_id"] == checkable_fields.file_id
+    assert payload["file_id"] == CONFIG.object_id
     assert payload["reason"] == "The crypt4GH envelope is either malformed or missing."
