33087199546f8a0498e56db563a84970e8e67146
Thomas J. Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2023-01-31 19:53:17 +0000

    IT'S ALIVE!

--- src/run_upload_path.py
+++ src/run_upload_path.py
@@ -15,8 +15,10 @@
 """This is... UPLOAD!!!"""
 
 import asyncio
+import base64
 import hashlib
 import os
+import sys
 import time
 from pathlib import Path
 from tempfile import NamedTemporaryFile
@@ -29,11 +31,7 @@ from ghga_event_schemas import pydantic_ as event_schemas
 from ghga_service_chassis_lib.utils import big_temp_file
 from hexkit.config import config_from_yaml
 from hexkit.providers.akafka import KafkaConfig, KafkaEventPublisher
-from hexkit.providers.mongodb import MongoDbConfig, MongoDbDaoFactory
 from hexkit.providers.s3 import S3Config, S3ObjectStorage
-from pydantic import BaseModel
-
-BASE_DIR = Path(__file__).parent.parent
 
 
 @config_from_yaml(prefix="tb")
@@ -47,72 +45,89 @@ class Config(S3Config, KafkaConfig):
     file_metadata_event_topic: str
     file_metadata_event_type: str
     inbox_bucket: str
-    submitter_pubkey: str
     vault_host: str
     vault_port: int
     vault_token: str
 
 
+BASE_DIR = Path(__file__).parent.parent
 CONFIG = Config()
+FILE_SIZE = 20 * 1024**2
+
+sys.set_int_max_str_digits(FILE_SIZE)
 
 
-class DBConfig:
-    """TODO: Could be an enum"""
-
-    dcs_config = MongoDbConfig(
-        db_connection_str=CONFIG.db_connection_str, db_name="dcs"
-    )
-    ifrs_config = MongoDbConfig(
-        db_connection_str=CONFIG.db_connection_str, db_name="ifrs"
-    )
-    irs_config = MongoDbConfig(
-        db_connection_str=CONFIG.db_connection_str, db_name="irs"
-    )
-    ucs_config = MongoDbConfig(
-        db_connection_str=CONFIG.db_connection_str, db_name="ucs"
-    )
+async def delegate_paths():
+    """
+    Generate and upload data for happy and unhappy paths
+    Return file IDs for later checks
+    """
+    unencrypted_data, encrypted_data, checksum = generate_file()
+    print("Uploading unencrypted file (unhappy path)")
+    unencrypted_id = await populate_data(data=unencrypted_data, checksum=checksum)
+    print("Uploading encrypted file (happy path)")
+    encrypted_id = await populate_data(data=encrypted_data, checksum=checksum)
+    return unencrypted_id, encrypted_id
 
 
-class UploadRejectedQuery(BaseModel):
-    """Query model for file upload rejection"""
-
-    id_: str
-    file_id: str
-    status: str
-
-
-class UCSMetadataQuery(BaseModel):
-    """Query model for IFRS validation"""
-
-    file_id: str
-    file_name: str
-
-
-def generate_file(file_size: int):
-    """Generate encrypted test file"""
-
-    with big_temp_file(size=file_size) as random_data:
+def generate_file():
+    """Generate encrypted test file, return both unencrypted and encrypted data as bytes"""
+    with big_temp_file(size=FILE_SIZE) as random_data:
         random_data.seek(0)
         data = random_data.read()
-        size = len(data)
         checksum = hashlib.sha256(data).hexdigest()
 
         with NamedTemporaryFile() as encrypted_file:
             random_data.seek(0)
             private_key = crypt4gh.keys.get_private_key(
-                filepath=BASE_DIR / "example_data" / "key.sec", callback=lambda: ()
+                filepath=BASE_DIR / "example_data" / "key.sec", callback=lambda: None
             )
-            public_key = crypt4gh.keys.get_public_key(
-                filepath=BASE_DIR / "example_data" / "key.pub"
-            )
-            encryption_keys = [(0, private_key, public_key)]
+            pub_key = base64.b64decode("qx5g31H7rdsq7sgkew9ElkLIXvBje4RxDVcAHcJD8XY=")
+            encryption_keys = [(0, private_key, pub_key)]
             crypt4gh.lib.encrypt(
                 keys=encryption_keys, infile=random_data, outfile=encrypted_file
             )
             encrypted_file.seek(0)
             encrypted_data = encrypted_file.read()
 
-            return data, encrypted_data, size, checksum
+            return data, encrypted_data, checksum
+
+
+async def populate_data(data: bytes, checksum: str):
+    """Populate events, storage and check initial state"""
+    file_id = os.urandom(16).hex()
+    await populate_metadata_and_upload(
+        file_id=file_id,
+        file_name=file_id,
+        data=data,
+        size=FILE_SIZE,
+        checksum=checksum,
+    )
+    await check_objectstorage(file_id=file_id)
+    return file_id
+
+
+async def populate_metadata_and_upload(
+    file_id: str, file_name: str, data: bytes, size: int, checksum: str
+):
+    """Generate and send metadata event, afterwards upload data to object storage"""
+    await populate_metadata(
+        file_id=file_id,
+        file_name=file_name,
+        decrypted_size=size,
+        decrypted_sha256=checksum,
+    )
+    # wait for possible delays in event delivery
+    time.sleep(15)
+    with NamedTemporaryFile() as tmp_file:
+        tmp_file.write(data)
+        tmp_file.flush()
+        tmp_file.seek(0)
+        upload(
+            file_id=file_id,
+            file_path=tmp_file.name,
+            pubkey_path=BASE_DIR / "example_data" / "key.pub",
+        )
 
 
 async def populate_metadata(
@@ -141,108 +156,16 @@ async def populate_metadata(
             key=key,
             topic=topic,
         )
-    time.sleep(15)
 
 
-async def populate_and_upload(
-    file_id: str, file_name: str, data: bytes, size: int, checksum: str
-):
-    """TODO"""
-    await populate_metadata(
-        file_id=file_id,
-        file_name=file_name,
-        decrypted_size=size,
-        decrypted_sha256=checksum,
-    )
-    with NamedTemporaryFile() as tmp_file:
-        tmp_file.write(data)
-        tmp_file.seek(0)
-        upload(
-            file_id=file_id,
-            file_path=tmp_file.name,
-            pubkey_path=BASE_DIR / "example_data" / "key.pub",
-        )
-
-
-async def delegate_paths():
-    """TODO"""
-    file_size = 20 * 1024**2
-    unencrypted_data, encrypted_data, size, checksum = generate_file(
-        file_size=file_size
-    )
-    await unhappy_path(data=unencrypted_data, size=size, checksum=checksum)
-    await happy_path(data=encrypted_data, size=size, checksum=checksum)
-
-
-async def unhappy_path(data: bytes, size: int, checksum: str):
-    """TODO"""
-    file_id = os.urandom(16).hex()
-    file_name = os.urandom(16).hex()
-    await populate_and_upload(
-        file_id=file_id, file_name=file_name, data=data, size=size, checksum=checksum
-    )
-    await check_unhappy_state(file_id=file_id, file_name=file_name)
-
-
-async def happy_path(data: bytes, size: int, checksum: str):
-    """TODO"""
-    file_id = os.urandom(16).hex()
-    file_name = os.urandom(16).hex()
-    await populate_and_upload(
-        file_id=file_id, file_name=file_name, data=data, size=size, checksum=checksum
-    )
-    await check_happy_state(file_id=file_id, file_name=file_name)
-
-
-async def check_unhappy_state(file_id: str, file_name: str):
-    """TODO"""
-    await check_s3(file_id=file_id)
-    internal_id = await check_metadata(file_name=file_name)
-    await check_rejected(internal_id=internal_id)
-
-
-async def check_happy_state(file_id: str, file_name: str):
-    """TODO"""
-    await check_s3(file_id=file_id)
-    inernal_id = await check_metadata(file_name=file_name)
-
-
-async def check_s3(file_id: str):
-    """TODO"""
+async def check_objectstorage(file_id: str):
+    """Check if object storage is populated"""
     storage = S3ObjectStorage(config=CONFIG)
     object_exists = await storage.does_object_exist(
         bucket_id=CONFIG.inbox_bucket, object_id=file_id
     )
     if not object_exists:
         raise ValueError("Object missing in inbox")
-
-
-async def check_metadata(file_name: str):
-    """TODO"""
-    dao_factory = MongoDbDaoFactory(config=DBConfig.ucs_config)
-    metadata_dao = await dao_factory.get_dao(
-        name="file_metadata", dto_model=UCSMetadataQuery, id_field="file_id"
-    )
-
-    result = await metadata_dao.find_one(mapping={"file_name": file_name})
-
-    # assert result
-    internal_id = result.file_id
-
-    return internal_id
-
-
-async def check_rejected(internal_id: str):
-    """TODO"""
-    time.sleep(20)
-    dao_factory = MongoDbDaoFactory(config=DBConfig.ucs_config)
-    upload_attempt_dao = await dao_factory.get_dao(
-        name="upload_attempts", dto_model=UploadRejectedQuery, id_field="id_"
-    )
-    result = await upload_attempt_dao.find_one(mapping={"file_id": internal_id})
-
-    # assert result
-    # assert result.status == "rejected"
 
 
 if __name__ == "__main__":
